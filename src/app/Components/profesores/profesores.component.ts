637fdb5d062f032b77cab198cc38c1274fba6c97
Jogeiker <jogeiker1999@gmail.com>
2019-09-04 23:30:51 -0400

    actualizaciones bergas

--- src/app/Components/profesores/profesores.component.ts
+++ src/app/Components/profesores/profesores.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { profesores } from 'src/assets/DB/profesores';
+
 
 @Component({
   selector: 'app-profesores',
@@ -7,9 +15,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfesoresComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  constructor(private fb: FormBuilder, private router:Router) { }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
+
+  displayedColumns: string[] = ['ci', 'nom_prof', 'ape_prof', 'correo_prof'];
+  dataSource = new MatTableDataSource(profesores);
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  profesoresForm  = new FormGroup({
+    ci_profesor: new FormControl('',[Validators.required,Validators.maxLength(8),Validators.pattern('^[0-9]{1,8}$')]),
+    nombre_profesor: new FormControl('',[Validators.required]),
+    apellido_profesor: new FormControl('',[Validators.required]),
+    correo_profesor: new FormControl('',[Validators.email,Validators.required]),
+    
+  });
+
+  get ci_profesor() {return this.profesoresForm.get('ci_profesor')}
+  get nombre_profesor() {return this.profesoresForm.get('nombre_profesor')}
+  get apellido_profesor() {return this.profesoresForm.get('apellido_profesor')}
+  get correo_profesor() {return this.profesoresForm.get('correo_profesor')}
+ 
+
+
+
+  guardarProfesor(){
+    profesores.push(this.profesoresForm.value)
+    
+    this.router.navigateByUrl('/reload', {skipLocationChange: true}).then(()=>
+    {
+      this.router.navigate(['/profesores'])
+      
+    }); 
+    
+  }
 }
---
+++ src/assets/DB/profesores.ts
@@ -0,0 +1,1 @@
+export const profesores = [{ci_profesor:'27040372',nombre_profesor:'Jogeiker',apellido_profesor:'Lizarraga',correo_profesor:'jogeiker1999@gmail.com'}]
